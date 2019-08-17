3d87695a090306c461d5977b7998c939b6e1fe26
furimako <furimako@gmail.com>
2019-08-17 12:42:19 +0900

    fix bug in logging (error)

--- src/logging.js
+++ src/logging.js
@@ -1,10 +1,6 @@
 const JST = require('./jst')
-const mailer = require('./mailer')
 
 module.exports = {
     info: msg => console.log(`${JST.getISO8610()} [INFO] ${msg}`),
-    error: (msg) => {
-        console.error(`${JST.getISO8610()} [ERROR] ${msg}`)
-        mailer.send('get ERROR', `${msg}`)
-    }
+    error: msg => console.error(`${JST.getISO8610()} [ERROR] ${msg}`)
 }
